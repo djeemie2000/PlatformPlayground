80e9cf175285502c50d989daa319e6216bc0c724
djeemie2000 <peter.jamee@telenet.be>
2018-10-06 17:14:49 +0200

    Mode midihandler : remove live mono mode, note off upon mode change from stepper to live.

--- MidiMultiCV/src/ModeMidiHandler.cpp
+++ MidiMultiCV/src/ModeMidiHandler.cpp
@@ -1,5 +1,126 @@
 #include "ModeMidiHandler.h"
 
+ModeMidiHandler::ModeMidiHandler(uint8_t Channel, MidiHandler& midiOut) 
+    : m_Channel(Channel)
+    , m_midiOut(midiOut)
+    , m_Mode(LivePoly)
+    , m_PrevGate(false)
+    , m_State()
+    , m_Stepper()
+  {}
+
+void ModeMidiHandler::Tick(int Gate)
+{
+    m_State.Tick();
+    m_Stepper.Tick(Gate);
+
+    if(m_Mode==StepperPlay || m_Mode == StepperRecord)
+    {
+        if(m_PrevGate != m_Stepper.Gate())
+        {
+            if(m_PrevGate)
+            {
+                m_midiOut.NoteOff(m_Channel, m_Stepper.MidiNote(), m_Stepper.Velocity());
+            }
+            else
+            {
+                m_midiOut.NoteOn(m_Channel, m_Stepper.MidiNote(), m_Stepper.Velocity());
+            }
+            m_PrevGate = !m_PrevGate;
+        }
+    }
+  }
+
+  void ModeMidiHandler::SetMode(Mode mode)
+  {
+      if(mode != m_Mode)
+      {
+        // when switching mode, make sure no notes get stuck
+        // e.g. live -> sequencer, sequencer -> live
+        // Note: a real note stack (highest note, latest note order, ...) would be usefull
+        if(m_Mode==StepperRecord || m_Mode == StepperPlay)
+        {
+            // current note off if a note is on
+            if(m_Stepper.Gate())
+            {
+                m_midiOut.NoteOff(m_Channel, m_Stepper.MidiNote(), m_Stepper.Velocity());
+            }
+        }
+
+        m_Mode = mode;
+
+      }
+
+  }
+
+
+  void ModeMidiHandler::NoteOn(uint8_t Channel, uint8_t MidiNote, uint8_t Velocity)
+  {
+     if(m_Channel == Channel)
+     {
+         m_State.NoteOn(MidiNote, Velocity);
+         if(m_Mode==LivePoly)
+         {
+             m_midiOut.NoteOn(Channel, MidiNote, Velocity);
+         } 
+         else if(m_Mode==StepperPlay)
+         {
+             m_Stepper.SetBaseNote(MidiNote);
+         }
+         else if(m_Mode==StepperRecord)
+         {
+              // long press => clear
+              // shorter press => add (high velocity) or skip (low velocity)
+              //    => need to wait for note off to be sure it is a clear or add
+              const uint8_t SkipStepThreshold = 48;//good threshold value?
+              if(Velocity<SkipStepThreshold)
+              {
+                  m_Stepper.AddSkipStep();
+              }
+              else
+              {
+                m_Stepper.AddStep(MidiNote, Velocity);
+              }
+         }
+     }
+  }
+
+ void ModeMidiHandler::NoteOff(uint8_t Channel, uint8_t MidiNote, uint8_t Velocity)
+ {
+    if(m_Channel == Channel)
+    {
+        m_State.NoteOff(MidiNote, Velocity);
+        if(m_Mode==LivePoly)
+        {
+            m_midiOut.NoteOff(Channel, MidiNote, Velocity);
+        }
+        else if(m_Mode==StepperRecord)
+        {
+            // long press => clear
+            // shorter press => add (high velocity) or skip (low velocity)
+            //    => cannot act upon note on, but need to wait for note off
+            // alternative: act in tick???  
+            if(1024<m_State.PressDuration(MidiNote))
+            {
+                // current note off if a note is on
+                if(m_Stepper.Gate())
+                {
+                    m_midiOut.NoteOff(m_Channel, m_Stepper.MidiNote(), m_Stepper.Velocity());
+                }
+                m_Stepper.Clear();
+            }
+        }
+    }
+ }
+
+ void ModeMidiHandler::ContinuousController(uint8_t Channel, uint8_t Controller, uint8_t Value)
+ {
+     if(m_Channel == Channel)
+     {
+         //TODO learn controller for gate length
+         m_midiOut.ContinuousController(Channel, Controller, Value);
+     }
+ }
 
 void ModeMidiHandler::PitchWheel(uint8_t Channel, int Bend)
 {
@@ -8,4 +129,3 @@ void ModeMidiHandler::PitchWheel(uint8_t Channel, int Bend)
         m_midiOut.PitchWheel(Channel, Bend);
     }
 }
-
--- MidiMultiCV/src/ModeMidiHandler.h
+++ MidiMultiCV/src/ModeMidiHandler.h
@@ -12,124 +12,33 @@ public:
     enum Mode
     {
         LivePoly,
-        LiveMono,
         StepperRecord,
         StepperPlay
     };
 
-  ModeMidiHandler(uint8_t Channel, MidiHandler& midiOut) 
-    : m_Channel(Channel)
-    , m_midiOut(midiOut)
-    , m_Mode(LivePoly)
-    , m_PrevGate(false)
-    , m_State()
-    , m_Stepper()
-  {}
+  ModeMidiHandler(uint8_t Channel, MidiHandler& midiOut);
 
-  void Tick(int Gate){
-      m_State.Tick();
-      m_Stepper.Tick(Gate);
+  void Tick(int Gate);
+  void SetMode(Mode mode);
 
-      if(m_Mode==StepperPlay || m_Mode == StepperRecord)
-      {
-          if(m_PrevGate != m_Stepper.Gate())
-          {
-              if(m_PrevGate)
-              {
-                  m_midiOut.NoteOff(m_Channel, m_Stepper.MidiNote(), m_Stepper.Velocity());
-              }
-              else
-              {
-                  m_midiOut.NoteOn(m_Channel, m_Stepper.MidiNote(), m_Stepper.Velocity());
-              }
-              m_PrevGate = !m_PrevGate;
-          }
-      }
-  }
-
-  void SetMode(Mode mode)
-  {
-      m_Mode = mode;
-      //TODO chen switching mode, make sure no notes get stuck
-      // e.g. live -> sequencer, sequencer -> live
-      // Note: a real note stack (highest note, latest note order, ...) would be usefull
-  }
-
-
-  void NoteOn(uint8_t Channel, uint8_t MidiNote, uint8_t Velocity) override
-  {
-     if(m_Channel == Channel)
-     {
-         m_State.NoteOn(MidiNote, Velocity);
-         if(m_Mode==LivePoly || m_Mode==LiveMono)
-         {
-             m_midiOut.NoteOn(Channel, MidiNote, Velocity);
-         } 
-         else if(m_Mode==StepperPlay)
-         {
-             m_Stepper.SetBaseNote(MidiNote);
-         }
-         else if(m_Mode==StepperRecord)
-         {
-              // long press => clear
-              // shorter press => add (high velocity) or skip (low velocity)
-              //    => cannot act upon note on, but need to wait for note off
-              // alternative: act in tick???  
-              if(Velocity<48)//TODO threshold value?
-              {
-                  m_Stepper.AddSkipStep();
-              }
-              else
-              {
-                m_Stepper.AddStep(MidiNote, Velocity);
-              }
-         }
-     }
-  }
- void NoteOff(uint8_t Channel, uint8_t MidiNote, uint8_t Velocity) override
- {
-     if(m_Channel == Channel)
-     {
-         m_State.NoteOff(MidiNote, Velocity);
-         if(m_Mode==LivePoly)
-         {
-             m_midiOut.NoteOff(Channel, MidiNote, Velocity);
-         }
-         else if (m_Mode==LiveMono)
-         {
-             if(0==m_State.NumPressed())
-             {
-                 m_midiOut.NoteOff(Channel, MidiNote, Velocity);
-             }
-         }
-         else if(m_Mode==StepperRecord)
-         {
-              // long press => clear
-              // shorter press => add (high velocity) or skip (low velocity)
-              //    => cannot act upon note on, but need to wait for note off
-              // alternative: act in tick???  
-              if(1024<m_State.PressDuration(MidiNote))
-              {
-                  // current note off if a note is on
-                  if(m_Stepper.Gate())
-                  {
-                      m_midiOut.NoteOff(m_Channel, m_Stepper.MidiNote(), m_Stepper.Velocity());
-                  }
-                  m_Stepper.Clear();
-              }
-         }
-     }
- }
- void ContinuousController(uint8_t Channel, uint8_t Controller, uint8_t Value)
- {
-     if(m_Channel == Channel)
-     {
-         //TODO learn controller for gate length
-         m_midiOut.ContinuousController(Channel, Controller, Value);
-     }
- }
-
- void PitchWheel(uint8_t Channel, int Bend) override;
+  void NoteOn(uint8_t Channel, uint8_t MidiNote, uint8_t Velocity) override;
+  void NoteOff(uint8_t Channel, uint8_t MidiNote, uint8_t Velocity) override;
+  void ContinuousController(uint8_t Channel, uint8_t Controller, uint8_t Value) override;
+  void PitchWheel(uint8_t Channel, int Bend) override;
+    void AfterTouch(uint8_t Channel, uint8_t MidiNote, uint8_t Pressure)
+    {
+        m_midiOut.AfterTouch(Channel, MidiNote, Pressure);
+    }
+    void ProgramChange(uint8_t Channel, uint8_t Program) 
+    {
+        m_midiOut.ProgramChange(Channel, Program);
+    }
+    void ChannelPressure(uint8_t Channel, uint8_t Pressure) 
+    {
+        m_midiOut.ChannelPressure(Channel, Pressure);
+    }
+    //TODO pass all other functions to handler!
+    void AllNotesOff();
 
 private:
   uint8_t m_Channel;
