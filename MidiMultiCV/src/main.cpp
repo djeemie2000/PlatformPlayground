48786011f9edf570ed8b3929a1cb873caf0502da
djeemie2000 <peter.jamee@telenet.be>
2018-10-22 08:31:27 +0200

    Midi mult CV: rearranged the pins cfr spreadsheet.

--- MidiMultiCV/src/main.cpp
+++ MidiMultiCV/src/main.cpp
@@ -28,21 +28,15 @@ int main()
 //  DigitalIn clockIn(PA_12);//external clock input
   Serial pc2(PA_9, PA_10); // tx, rx
 
-  // outputs for CV out
-  DigitalOut gateOut(PA_11);//gate output TODO PB10 next to pwm's
-  PwmVoltageOut voltageOut(PB_1);//PWM voltage output for 1V/oct
-  PwmVoltageOut voltageOut2(PB_0);//PWM voltage output for velocity
-
   //clock output
   PeriodicOutState clockState;
-  DigitalOut clockOut(PB_3);
+  DigitalOut clockOut(PB_13);
 
-  
   // mode inputs (uses analog in)
   const int numModes = 3;
-  NPositionIn modeIn1(A0, numModes);
-  NPositionIn modeIn2(A1, numModes);
-  NPositionIn modeIn3(A4, numModes);
+  NPositionIn modeIn1(A4, numModes);
+  NPositionIn modeIn2(A5, numModes);
+  NPositionIn modeIn3(A6, numModes);
 
   // clock speed in
   AnalogIn clockSpeedIn(A7);
@@ -72,47 +66,39 @@ int main()
   pc2.printf("create channel 1\r\n");
   LogMidiHandler logHandler1(pc2, 1);
   SerialMidiHandler midiHandler1(pcMidi);
-  DigitalOut gateLed1(PA_12);
+  DigitalOut gateLed1(PB_1);
   GateMidiHandler midiGate1(gateLed1);
   MultiMidiHandler midiMulti1(logHandler1, midiHandler1, midiGate1, midiDummy);
   ModeMidiHandler modeHandler1(0, midiMulti1);
   modeHandler1.SetMode(ModeMidiHandler::LivePoly);
 
-  // channel 2: live to (mono)  CV
+  // channel 2: live to (mono)  CV + midi serial
   wait_ms(500);
   pc2.printf("create channel 2\r\n");
   LogMidiHandler logHandler2(pc2, 2);
-  CVMidiHandler midiHandler2(voltageOut, voltageOut2, gateOut);
+  // outputs for CV out channel 2
+  DigitalOut gateOut2(PB_4);//gate output TODO PB10 next to pwm's
+  PwmVoltageOut voltageOutPitch2(PB_6);//PWM voltage output for 1V/oct
+  PwmVoltageOut voltageOutVelocity2(PB_7);//PWM voltage output for velocity
+  CVMidiHandler midiHandler2(voltageOutPitch2, voltageOutVelocity2, gateOut2);
   MonophonicMidiHandler midiMono2(midiHandler2);
-  DigitalOut gateLed2(PA_15);
+  DigitalOut gateLed2(PB_10);
   GateMidiHandler midiGate2(gateLed2);
+  //TODO midi serial
   MultiMidiHandler midiMulti2(logHandler2, midiMono2, midiGate2, midiDummy);
   ModeMidiHandler modeHandler2(1, midiMulti2);
   modeHandler2.SetMode(ModeMidiHandler::LivePoly);
 
-  // channel 3: stepper to midi serial
+  // channel 3: stepper to midi serial and (mono) CV out 
   wait_ms(500);
   pc2.printf("create channel 3\r\n");
   LogMidiHandler logHandler3(pc2, 3);
   SerialMidiHandler midiHandler3(pcMidi);
-  DigitalOut gateLed3(PB_4);
+  DigitalOut gateLed3(PB_11);
   GateMidiHandler midiGate3(gateLed3);
   MultiMidiHandler midiMulti3(logHandler3, midiHandler3, midiGate3, midiDummy);
   ModeMidiHandler modeHandler3(2, midiMulti3);
   modeHandler3.SetMode(ModeMidiHandler::StepperRecord);
-
-  // channel 4 : todo
-/*  int cntr = 0;
-  while(true)
-  {
-    wait_ms(1000);
-    pc2.printf("version 0.3\r\n");
-    ledOut = 1;
-    wait_ms(1000);
-    pc2.printf("%d \r\n", cntr++);
-    ledOut = 0;
-  }
-*/ 
 
   wait_ms(500);
   pc2.printf("create multi\r\n");
