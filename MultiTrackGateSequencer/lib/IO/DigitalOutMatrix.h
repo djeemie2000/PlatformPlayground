8a4d2b77535b52080512546d27da195e6dc14290
djeemie2000 <peter.jamee@telenet.be>
2018-11-10 09:41:28 +0100

    Digital out matrix abstraction.

---
+++ MultiTrackGateSequencer/lib/IO/DigitalOutMatrix.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <mbed.h>
+
+// "Interface"
+class DigitalOutMatrix
+{
+public:
+    DigitalOutMatrix(){}
+
+    virtual int NumRows() const{return 1;}
+    virtual int NumCols() const{return 1;}
+    virtual void Set(int row, int col) {}
+    virtual void Clear(int row, int col) {}
+    virtual void write(){}
+};
