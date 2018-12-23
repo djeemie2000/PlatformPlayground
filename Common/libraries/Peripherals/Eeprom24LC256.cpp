29f9b3d046366904efc79906b1118eedad46f69e
djeemie2000 <peter.jamee@telenet.be>
2018-12-23 20:31:12 +0100

    Add support for eeprom 24LC256.

---
+++ Common/libraries/Peripherals/Eeprom24LC256.cpp
@@ -0,0 +1,47 @@
+#include "Eeprom24LC256.h"
+
+Eeprom24LC256::Eeprom24LC256(I2C* i2c, Address address)
+ : m_I2c(i2c)
+ , m_Address(address)
+{}
+
+void Eeprom24LC256::WriteAdress(int eepromAddress)
+{
+    //write address
+    uint8_t buffer[2];
+    buffer[0] = (eepromAddress >> 8); // MSB
+    buffer[1] = (eepromAddress & 0xFF); // LSB
+    m_I2c->write(m_Address, (char*)buffer, 2, false);
+}
+
+int Eeprom24LC256::writeBank(int bank, const uint8_t* data, int size)
+{
+    // no check on bank
+
+    int eepromAddress = bank<<6;//*64
+    WriteAdress(eepromAddress);
+
+    // write data, but crop write size to bank
+    int croppedSize = size<BankSize()?size:BankSize();
+    m_I2c->write(m_Address, (char*)data, croppedSize);
+    
+    //returns actual # written
+    return croppedSize;
+
+    //TODO prevent write for approx 5 milliseconds after a write
+    // but without blocking the whole application
+    // wait_ms(6);
+}
+
+int Eeprom24LC256::readBank(int bank, uint8_t* data, int size)
+{
+    int eepromAddress = bank<<6;//*64
+    WriteAdress(eepromAddress);
+
+    // read data, but crop write size to bank
+    int croppedSize = size<BankSize()?size:BankSize();
+    m_I2c->read(m_Address, (char*)data, croppedSize);
+    
+    //returns actual # read
+    return croppedSize;
+}
---
+++ Common/libraries/Peripherals/Eeprom24LC256.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <mbed.h>
+
+class Eeprom24LC256
+{
+public:
+    enum Address{
+        AllLow = 0xA0,
+        A0High =  0xA2,
+        A1High =  0xA4,
+        A2High =  0xA8,
+        A0A1High =  0xA6,
+        A0A2High =  0xAA,
+        A1A2High =  0xAC,        
+        AllHigh = 0xAE
+    };
+    Eeprom24LC256(I2C* i2c, Address address);
+    //TODO prevent write for approx 5 milliseconds after a write
+
+    int BankSize() const    {   return 64;      }
+    int NumBanks() const    {   return 4096;    }
+    int writeBank(int bank, const uint8_t* data, int size);//returns actual # written
+    int readBank(int bank, uint8_t* data, int size);//returns actual # read
+    //TODO offset support in bank
+
+private:
+    void WriteAdress(int eepromAddress);
+
+    I2C* m_I2c;
+    int m_Address;
+};
