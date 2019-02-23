47966452cf44359703aa4503b61986fce015ba51
djeemie2000 <peter.jamee@telenet.be>
2019-02-23 19:06:00 +0100

    Time based envelope.

--- MultiStageEnvelope/src/main.cpp
+++ MultiStageEnvelope/src/main.cpp
@@ -34,6 +34,76 @@ bool Stage::Update(int &value) {
   return advance;
 }
 
+class TimeBasedEnvelope {
+public:
+  static const int SkipDuration = 0;
+  static const int HoldDuration = -1;
+
+  TimeBasedEnvelope()
+      : m_EnvelopeValue(0.0f), m_ElapsedTime(0), m_CurrentStage(1),
+        m_PreviousStage(0) {
+    for (int idx = 0; idx < NumStages; ++idx) {
+      m_Level[idx] = idx % 2 ? 1.0f : 0.0f;
+      m_Duration[idx] = 1024;
+    }
+  }
+
+  // TODO update level/duration from CV input
+  void SetLevel(float level, int stage) { m_Level[stage] = level; }
+
+  // TODO update level/duration from CV input
+  void SetDuration(int duration, int stage) { m_Duration[stage] = duration; }
+
+  void Tick(int gate) {
+    // TODO duration based on gate and current stage
+
+    ++m_ElapsedTime;
+
+    int remainingTime = m_Duration[m_CurrentStage] - m_ElapsedTime;
+    if (remainingTime <= 0) {
+      //  advance stage
+      m_ElapsedTime = 0;
+      m_PreviousStage = m_CurrentStage;
+      ++m_CurrentStage;
+      if (NumStages <= m_CurrentStage) {
+        m_CurrentStage = 0;
+      }
+      remainingTime = m_Duration[m_CurrentStage];
+    }
+
+    if (SkipDuration == m_Duration[m_CurrentStage]) {
+      // immediate/skip
+      // => envelope value unaffected, handle next stage in next Tick() call
+    } else if (HoldDuration == m_Duration[m_CurrentStage]) {
+      // HOLD
+      // => envelope value equal to previous stage level
+      // (cfr sustain in ADSR)
+      m_EnvelopeValue = m_Level[m_PreviousStage];
+    } else {
+      // increase / decrease envelope based on line towards current stage level
+      float slope = (m_Level[m_CurrentStage] - m_EnvelopeValue) / remainingTime;
+      m_EnvelopeValue += slope;
+      //  crop envelope value to [0,1]
+      if (m_EnvelopeValue < 0) {
+        m_EnvelopeValue = 0.0f;
+      } else if (1 < m_EnvelopeValue) {
+        m_EnvelopeValue = 1.0f;
+      }
+    }
+  }
+
+private:
+  static const int NumStages = 4;
+
+  float m_Level[NumStages];  // (target) level [stage]
+  int m_Duration[NumStages]; // duration[0] = duration between stage 0  and 1,
+                             // etc...
+  float m_EnvelopeValue;
+  int m_ElapsedTime;
+  int m_CurrentStage;
+  int m_PreviousStage;
+};
+
 int main() {
 
   // put your setup code here, to run once:
