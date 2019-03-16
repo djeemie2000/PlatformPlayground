ac161c0cc3dd870740beb60162ab2417616afcfd
djeemie2000 <peter.jamee@telenet.be>
2019-03-16 21:47:25 +0100

    CVClok: 2nd clock.

--- CVClock/src/main.cpp
+++ CVClock/src/main.cpp
@@ -100,6 +100,7 @@ void CVClock::debugOut(uint32_t elapsed)
 }
 
 CVClock g_Clock(2, 3, A0);
+CVClock g_Clock2(4, 5, A1);
 int g_DebugCntr;
 uint32_t g_DebugMillis;
 
@@ -110,6 +111,7 @@ void setup()
   Serial.println("CVClock start...");
 
   g_Clock.Begin();
+  g_Clock2.Begin();
 
   g_DebugCntr = 0;
   g_DebugMillis = 0;
@@ -118,7 +120,9 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
+  // TODO alternate?
   g_Clock.Tick();
+  g_Clock2.Tick();
   delay(1);
 
   // debug...
@@ -127,6 +131,7 @@ void loop()
   {
     uint32_t time = millis();
     g_Clock.debugOut(time - g_DebugMillis);
+    g_Clock2.debugOut(time - g_DebugMillis);
     g_DebugMillis = time;
     g_DebugCntr = 0;
   }
