926f76ac6e872115b6a00b53af7fc8e0f754be30
djeemie2000 <peter.jamee@telenet.be>
2019-06-28 11:13:38 +0200

    CVClock: change pins, multiple targets: arduino uno + nano.

--- CVClock/src/main.cpp
+++ CVClock/src/main.cpp
@@ -10,23 +10,25 @@ CVClock g_Clock5;
 int g_DebugCntr;
 uint32_t g_DebugMillis;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("CVClock 0.2 start...");
+  Serial.println("CVClock 0.3 start...");
 
-  g_Clock.Begin(2, 3, A0);
-  g_Clock2.Begin(4, 5, A1);
-  g_Clock3.Begin(6, 7, A2);
-  g_Clock4.Begin(8, 9, A3);
-  g_Clock5.Begin(10, 11, A4);
+  g_Clock.Begin(11, 2, A4);
+  g_Clock2.Begin(10, 3, A3);
+  g_Clock3.Begin(9, 4, A2);
+  g_Clock4.Begin(8, 5, A1);
+  g_Clock5.Begin(7, 6, A0);
   pinMode(12, INPUT_PULLUP);
 
   g_DebugCntr = 0;
   g_DebugMillis = 0;
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   // TODO alternate analog reads?
   g_Clock.ReadDuration();
@@ -42,9 +44,11 @@ void loop() {
   //  delay(1);//not needed
 
   //  only debug if debug input is low (pullup digital in)
-  if (!digitalRead(12)) {
+  if (!digitalRead(12))
+  {
     ++g_DebugCntr;
-    if (1000 < g_DebugCntr) {
+    if (1000 < g_DebugCntr)
+    {
       // TODO stopwatch class
       Serial.println();
       Serial.println("-");
