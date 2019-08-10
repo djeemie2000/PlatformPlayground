e5adb92fe817551a428098939d0214a3f5a875f5
djeemie2000 <peter.jamee@telenet.be>
2019-08-10 12:00:24 +0200

    Key8: basic functionality : touchpad pads [0-7] to 3bit selected + gate out.

---
+++ Key8/src/CapacitiveTouchPad.h
@@ -0,0 +1,35 @@
+#ifndef CCAPACITIVETOUCHPAD_H
+#define CCAPACITIVETOUCHPAD_H
+
+#include <Arduino.h>
+
+class CapacitiveTouchPad
+{
+public:
+    static const uint8_t DefaultAddress = 0x5A;
+    static const uint8_t DefaultTouchThreshold = 0x06;
+    static const uint8_t DefaultReleaseThreshold = 0x0A;
+
+    CapacitiveTouchPad();
+
+    void Begin(int IrqPin,
+                uint8_t TouchThreshold = DefaultTouchThreshold,
+                uint8_t ReleaseThreadhold = DefaultReleaseThreshold,
+                uint8_t Address = DefaultAddress);
+    // TODO return true/false upon I2C device found or not !
+
+    void Read();
+    int GetNumPads() const;
+    bool Get(int Pad) const;
+    bool IsPushed(int Pad) const;
+    bool IsClicked(int Pad) const;
+    bool IsReleased(int Pad) const;
+
+private:
+    int     m_IrqPin;
+    uint8_t m_Address;
+    uint16_t m_TouchState;// bit = 1 => touched, bit is 0 => not touched
+    uint16_t m_PrevTouchState;
+};
+
+#endif // CCAPACITIVETOUCHPAD_H
---
+++ Key8/src/main.cpp
@@ -0,0 +1,137 @@
+#include <Arduino.h>
+#include "CapacitiveTouchPad.h"
+
+CapacitiveTouchPad g_TouchPad;
+
+struct TouchState
+{
+  int numPressed;
+  int selectedPad;
+
+  TouchState() 
+  : numPressed(0)
+  , selectedPad(0)
+  {}
+};
+
+TouchState g_TouchState;
+
+class TouchStateOut
+{
+public:
+  TouchStateOut() {}//undefined pins!
+
+  void Begin(int gatePin, int bit0Pin, int bit1Pin, int bit2Pin)
+  {
+    m_GateOutPin = gatePin;
+    m_Bit0Pin = bit0Pin;
+    m_Bit1Pin = bit1Pin;
+    m_Bit2Pin = bit2Pin;
+
+    pinMode(m_GateOutPin, OUTPUT);
+    pinMode(m_Bit0Pin, OUTPUT);
+    pinMode(m_Bit1Pin, OUTPUT);
+    pinMode(m_Bit2Pin, OUTPUT);
+  }
+
+  void Write(const TouchState& touchState)
+  {
+    digitalWrite(m_GateOutPin, 0<touchState.numPressed?1:0);
+    digitalWrite(m_Bit0Pin, (touchState.selectedPad&1)?1:0);
+    digitalWrite(m_Bit1Pin, (touchState.selectedPad&2)?1:0);
+    digitalWrite(m_Bit2Pin, (touchState.selectedPad&4)?1:0);
+  }
+
+  private:
+    int m_GateOutPin;
+    int m_Bit0Pin;
+    int m_Bit1Pin;
+    int m_Bit2Pin;
+};
+
+TouchStateOut g_TouchStateOut;
+
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(115200);
+ Serial.println("Key8 v0.1...");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  g_TouchPad.Begin(PIND2);//IRQ pin D2
+  g_TouchStateOut.Begin(11, 8, 9, 10);
+}
+
+void blinkLed(int period)
+{
+  digitalWrite(LED_BUILTIN, 1);
+  Serial.println("Led ON");
+  delay(period);
+  digitalWrite(LED_BUILTIN, 0);
+  Serial.println("Led OFF");
+  delay(period);
+}
+
+void testTouchPad(CapacitiveTouchPad& touchPad)
+{
+  for(int pad = 0; pad<touchPad.GetNumPads(); ++pad)
+  {
+    if(touchPad.IsClicked(pad))
+    {
+      Serial.print(pad);
+      Serial.println(" was clicked");
+    }
+    else if(touchPad.IsReleased(pad))
+    {
+      Serial.print(pad);
+      Serial.println(" was released");
+    }
+  }
+}
+
+void updateTouchState(CapacitiveTouchPad& touchPad, TouchState& touchState)
+{
+  // call read elsewhere?  touchPad.Read();
+  touchState.numPressed = 0;//reset num pressed count
+  // hold selected pad when none are pushed
+  // highest pad priority => count up and set selected
+  for(int pad = 0; pad<8; ++pad)//limit to pad [0-7]
+  {
+    if(touchPad.IsPushed(pad))
+    {
+      ++touchState.numPressed;
+      touchState.selectedPad = pad;
+    }
+  }
+}
+
+void debugTouchState(const TouchState& touchState)
+{
+  Serial.print(touchState.numPressed);
+  Serial.print(" pressed, current=");
+  Serial.println(touchState.selectedPad);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  blinkLed(300);
+  blinkLed(300);
+  blinkLed(800);
+
+  int debugCounter = 0;
+  while(true)
+  {
+   g_TouchPad.Read();
+//   testTouchPad(g_TouchPad);
+   updateTouchState(g_TouchPad, g_TouchState);
+   g_TouchStateOut.Write(g_TouchState);
+
+   ++debugCounter;
+   if(500<debugCounter)
+   {
+     debugTouchState(g_TouchState);
+     debugCounter = 0;
+   }
+   delay(1);// TODO what delay is needed??
+  }
+}
