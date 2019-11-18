c1b94372327c48d155e13b7c77c2d846fe7391c8
djeemie2000 <peter.jamee@telenet.be>
2019-11-18 21:46:37 +0100

    DigiNoise : added random.

--- DigiNoise/src/main.cpp
+++ DigiNoise/src/main.cpp
@@ -1,6 +1,7 @@
 #include "library/include/attiny.h"
 #include "library/include/attiny_digital.h"
 #include "library/include/attiny_sleep.h"
+#include "library/include/attiny_random.h"
 
 #define	LED_PIN		PB0
 
@@ -8,6 +9,8 @@ void setup() {
   // put your setup code here, to run once:
   pin_mode(LED_PIN, OUTPUT);
 	digital_write(LED_PIN, LOW);
+
+  random_init(500);//TODO random from EEPROM!!!
 }
 
 void loop() {
@@ -16,6 +19,10 @@ void loop() {
 	sleep(300);
   digital_toggle(LED_PIN);
 	sleep(300);
+  
   digital_toggle(LED_PIN);
 	sleep(800);
+
+  digital_toggle(LED_PIN);
+  sleep(random()>>6);//range 2^10 = 1024 msec
 }
