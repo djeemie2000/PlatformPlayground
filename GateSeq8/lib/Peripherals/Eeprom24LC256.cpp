c82b31eeaa98b839d829ce2be5cb71afee937ff4
djeemie2000 <peter.jamee@telenet.be>
2020-02-23 08:38:46 +0100

    GateSeq8 : cleanup memory bank code.

--- GateSeq8/lib/Peripherals/Eeprom24LC256.cpp
+++ GateSeq8/lib/Peripherals/Eeprom24LC256.cpp
@@ -13,11 +13,7 @@ void Eeprom24LC256::WriteEepromAdress(int eepromAddress)
     buffer[0] = (eepromAddress >> 8); // MSB
     buffer[1] = (eepromAddress & 0xFF); // LSB
 
-//    m_I2c->write(m_Address, (char*)buffer, 2, false);
-//    Wire.beginTransmission(m_Address);
-    Wire.write(buffer[0]);
-    Wire.write(buffer[1]);    
-//    Wire.endTransmission();// continue transmission in write or read
+    Wire.write(buffer, 2);
 }
 
 int Eeprom24LC256::WriteBank(int bank, const uint8_t* data, int size, int offset)
@@ -28,27 +24,15 @@ int Eeprom24LC256::WriteBank(int bank, const uint8_t* data, int size, int offset
 
         // no check on bank
         int eepromAddress = bank*BankSize()+offset;
-
         // write data, but crop write size to bank
-        int croppedSize = min(size, BankSize()-offset);//size<BankSize()?size:BankSize();
+        int croppedSize = min(size, BankSize()-offset);
 
         Wire.beginTransmission(m_Address);
         WriteEepromAdress(eepromAddress);
-        //Wire.write(data,croppedSize);
-        const uint8_t* d = data;
-        while(d<data+croppedSize)
-        {
-            Wire.write(*d);
-            ++d;
-        }
+        int numWritten = Wire.write(data,croppedSize);
         Wire.endTransmission();
-    
-        //TODO prevent write for approx 5 milliseconds after a write
-        // but without blocking the whole application
-//        delay(20);
-
         //returns actual # written
-        return croppedSize;
+        return numWritten;
     }
     return 0;
 }
@@ -61,29 +45,15 @@ int Eeprom24LC256::ReadBank(int bank, uint8_t* data, int size, int offset)
 
         // no check on bank
         int eepromAddress = bank*BankSize()+offset;
+        // read data, but crop read size to bank
+        int croppedSize = min(size, BankSize()-offset);
 
         Wire.beginTransmission(m_Address);
         WriteEepromAdress(eepromAddress);
         Wire.endTransmission();//??
 
-        // read data, but crop write size to bank
-        int croppedSize = min(size, BankSize()-offset);//size<BankSize()?size:BankSize();
         Wire.requestFrom(m_Address,croppedSize);
-        //return Wire.readBytes(data, croppedSize);
-
-        int numRead = 0;
-        uint8_t* d = data;
-        while(Wire.available())
-        {
-        // read one byte, advance readTo pointer
-        *d = Wire.read();
-        ++d;
-        ++numRead;
-        }
-        return numRead;
-        
-        //returns actual # read
-        //return croppedSize;
+        return Wire.readBytes(data, croppedSize);
     }
     return 0;
 }
@@ -91,7 +61,6 @@ int Eeprom24LC256::ReadBank(int bank, uint8_t* data, int size, int offset)
 /*
 	Acknowledge Pooling Algoritihm recomended by Manufacturer	
 */
-
 void Eeprom24LC256::ack_pooling()
 {
 	uint8_t code = -1;
