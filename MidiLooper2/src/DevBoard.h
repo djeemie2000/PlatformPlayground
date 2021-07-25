3548e60d3b322c708b19db0446ea9780652d4e31
djeemie2000 <peter.jamee@telenet.be>
2021-07-25 09:57:17 +0200

    MidiLooper2: devboard in separate header.

---
+++ MidiLooper2/src/DevBoard.h
@@ -0,0 +1,104 @@
+#pragma once
+#include <Arduino.h>
+#include "MPR121TouchPad.h"
+#include "Max7219Matrix.h"
+#include "DigitalIn.h"
+#include "DigitalInBank.h"
+
+
+struct DevBoard
+{
+  static const int LedPin = 2;
+  static const int csPinLedMatrix = 5;
+  static const int irqPinTouchPad = 15;
+  static const int debugPin = 4;// TODO debugIn DIn
+
+
+  // uart0 : usb + GPIO 1 & 3
+  // uart1 : not connected
+  // uart2 : GPIO 16 & 17
+  HardwareSerial serialDebug;//uart 0
+  bool debugIsActive;
+
+  HardwareSerial serialMidi;// uart 2
+  // TODO membank
+  DigitalInBank IOXP1;
+//  DigitalOutBank IOXP2;
+//  DigitalOutBank IOXP3;
+//  AnalogIn Pot1;
+//  AnalogIn Pot2;
+//  AnalogIn Pot3;
+  MPR121TouchPad MPR121A;
+  Max7219Matrix ledMatrix;//(1,PA4);//cs pin
+
+  DevBoard()
+   : serialDebug(0)
+   , debugIsActive(false)
+   , serialMidi(2)
+   , IOXP1(34, 35)
+  //  , IOXP2(PB3, PA15, PA12, PA11)
+  //  , IOXP3(PB7, PB6, PB5, PB4)
+  //  , Pot1()
+  //  , Pot2()
+  //  , Pot3()
+   , MPR121A()
+   , ledMatrix(1, csPinLedMatrix)//cs pin
+   {}
+
+   void Begin()
+   {
+     pinMode(LedPin, OUTPUT);// led builtin
+     pinMode(debugPin, INPUT_PULLUP);
+
+     serialDebug.begin(115200);
+     serialMidi.begin(31250);
+
+     IOXP1.begin();
+    //  IOXP2.begin();
+    //  IOXP3.begin();
+
+    //  Pot1.begin(PA0);
+    //  Pot2.begin(PA1);
+    //  Pot3.begin(PA2);
+
+     // setup I2C for MPR121 MPR121A
+     // Wire.begin() is called inside MPR121A.begin()
+     MPR121A.Begin(irqPinTouchPad); //irq pin
+     //TODO autoDetect I2C : MPR121A memBank
+
+      //SPI CS external
+      ledMatrix.Configure();     
+   }
+
+   void update()
+   {
+     //TODO!!!!!!!!!!!!!!
+    debugIsActive = true;//TODO!!!(0 == digitalRead(debugPin));
+    IOXP1.update();
+    // IOXP2.update();
+    // IOXP3.update();
+
+    //  Pot1.Read();
+    //  Pot2.Read();
+    //  Pot3.Read();
+    // update led matrix, touchpad here??
+    MPR121A.Read();
+    ledMatrix.WriteAll();
+   }
+
+   //TODO update() -> store in variable
+   bool debugActive() const
+   {
+     return debugIsActive; 
+   }
+
+   void LedOn()
+   {
+     digitalWrite(LedPin, HIGH);
+   }
+
+   void LedOff()
+   {
+     digitalWrite(LedPin, LOW);
+   }
+};
