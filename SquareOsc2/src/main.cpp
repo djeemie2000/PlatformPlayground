a5127cd5ef85bc6c23205abcd79651782167af95
djeemie2000 <peter.jamee@telenet.be>
2021-11-27 14:21:43 +0100

    SquareOsc2: poc using ticker at high frequencies.

--- SquareOsc2/src/main.cpp
+++ SquareOsc2/src/main.cpp
@@ -1,25 +1,61 @@
 #include <mbed.h>
 
+DigitalOut led(LED1);
+Ticker ticker;
+
+DigitalIn gateIn(PB_14, PullUp);
+DigitalOut oscOut(PB_15);
+uint32_t oscPeriod;
+uint32_t oscCntr;
+
+void ledOff()
+{
+  led = 0;
+}
+
+void ledOn()
+{
+  led = 1;
+}
+
+void toggleOsc()
+{
+  int gate = gateIn;
+  ++oscCntr;
+  if(oscCntr >= oscPeriod)
+  {
+    oscCntr = 0;
+    if(gate)
+    {
+      oscOut = !oscOut;
+    }
+    else 
+    {
+      oscOut = 0;
+    }
+  }
+}
+
 int main() {
 
   // put your setup code here, to run once:
-  DigitalOut led(LED1);
+  oscPeriod = 1;
+  oscCntr = 0;
+
+  us_timestamp_t period_us = 200;// 5KHz
+  ticker.attach_us(toggleOsc, period_us);
 
   while(1) {
-    // put your main code here, to run repeatedly:
-    led.write(1);
-    wait(0.4);
-    led.write(0);
-    wait(0.4);
-
-    for(int repeat = 0; repeat<2; ++repeat)
+    //put your main code here, to run repeatedly:
+    ledOn();
+    wait(0.7);
+    ledOff();
+    wait(0.7);
+    ++oscPeriod;
+    if(oscPeriod>16)
     {
-      led.write(1);
-      wait(0.2);
-      led.write(0);
-      wait(0.2);
+      oscPeriod = 1;
     }
-
   }
 }
 
