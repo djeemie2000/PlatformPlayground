7a8e07de9ecb0ed44908f33c79732a76162b483a
djeemie2000 <peter.jamee@telenet.be>
2022-05-09 12:30:53 +0200

    Midi clock generator: use time, 1 msec resolution.

--- MidiClockGenerator/src/main.cpp
+++ MidiClockGenerator/src/main.cpp
@@ -2,6 +2,12 @@
 #include "fastdigitalwrite.h"
 #include "clockoutstate.h"
 
+#define USE_TIMER_1     true
+#define USE_TIMER_2     false
+#define USE_TIMER_3     false
+
+#include "TimerInterrupt.h"
+
 // send serial midi start / clock / stop at some interval e.g. 20 msec 
 // -> 20 msec x 24 PPQ = 480 msec approx 2 beats per second approx 120BPM 
 
@@ -17,11 +23,8 @@
 // TODO timerinterrupt library
 
 
-
-//loop state
-int clockPeriod24PPQ;
-
 // shared state between loop and interrupt
+int clockPeriod24PPQ;
 int running;
 
 // interrupt state
@@ -30,45 +33,7 @@ int prevRunning;
 ClockOutState<int> clockOutStateSixteenthNotes;
 ClockOutState<int> ledOutStateTempoIndicator;
 
-void setup() 
-{
-  // put your setup code here, to run once:
 
-  // serial port -> midi -> 31250 baudrate
-  Serial.begin(31250);
-
-  // digital in/out pins:
-  pinMode(A0, INPUT_PULLUP);
-  // A1 pot analog read input
-  pinMode(A2, OUTPUT); // 16th note clock output
-  pinMode(A3, OUTPUT); // tempo led output
-  pinMode(LED_BUILTIN, OUTPUT);
-  
-  // -> 20 msec x 24 PPQ = 480 msec 
-  // approx 2 beats per second 
-  // approx 120BPM 
-  clockPeriod24PPQ = 20;
-  running = 0;
-
-  interruptCounter = 0;
-  prevRunning = 0;
-  // at 24 PPQ, period = 6 => 4 PPQ => quarter note / 4  = sixteenth notes clock
-  clockOutStateSixteenthNotes.Configure(3,6);
-
-  // quarter note led blink
-  ledOutStateTempoIndicator.Configure(12,24);
-  
-  //TODO upon midi start, singleshot reset pulse  length = one 16th note clock pulse
-
-  //flash led upon start??
-  for(int repeat = 0; repeat<4; ++repeat)
-  {
-    fastDigitalWritePortB<5>(1);
-    delay(200);
-    fastDigitalWritePortB<5>(0);
-    delay(200);
-  }
-}
 
 void oninterrupt()
 {
@@ -87,8 +52,6 @@ void oninterrupt()
   else if(prevRunning && !running)
   {
     Serial.write(0xFC);//midi stop
-    //clock out off
-    digitalWrite(A2, 0);
   }
   
   if(running && interruptCounter == 0)
@@ -102,18 +65,71 @@ void oninterrupt()
   fastDigitalWritePortC<2>(clockValue);
 
   int tempoLedValue = running ? ledOutStateTempoIndicator.Get() : 0;
-  fastDigitalWritePortC<3>(tempoLedValue);
+  fastDigitalWritePortC<4>(tempoLedValue);
 
   // led on if running, off if not
   fastDigitalWritePortB<5>(running);
 
   prevRunning = running;
 
-  //20 msec fixed delay
   ++interruptCounter;
   if(clockPeriod24PPQ<interruptCounter)
   {
     interruptCounter = 0;
+  }
+}
+
+int testonoff;
+
+void oninterrupttest()
+{
+  fastDigitalWritePortC<3>(testonoff);
+  testonoff = 1-testonoff;//toggle 0/1
+}
+
+void setup() 
+{
+  // put your setup code here, to run once:
+
+  // serial port -> midi -> 31250 baudrate
+  Serial.begin(31250);
+
+  // digital in/out pins:
+  pinMode(A0, INPUT_PULLUP);
+  // A1 pot analog read input
+  pinMode(A2, OUTPUT); // 16th note clock output
+  pinMode(A3, OUTPUT); // reset pulse output
+  pinMode(A4, OUTPUT); // tempo led output
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // -> 20 msec x 24 PPQ = 480 msec 
+  // approx 2 beats per second 
+  // approx 120BPM 
+  clockPeriod24PPQ = 20;
+  running = 0;
+
+  interruptCounter = 0;
+  prevRunning = 0;
+  // at 24 PPQ, period = 6 => 4 PPQ => quarter note / 4  = sixteenth notes clock
+  clockOutStateSixteenthNotes.Configure(3,6);
+
+  // quarter note led blink
+  ledOutStateTempoIndicator.Configure(12,24);
+  
+  //TODO upon midi start, singleshot reset pulse  length = one 16th note clock pulse
+
+  // start timer with period 1 msec
+  testonoff = 0;
+  ITimer1.init();
+  ITimer1.attachInterruptInterval(1, oninterrupt); 
+
+  //flash led upon start??
+  for(int repeat = 0; repeat<4; ++repeat)
+  {
+    fastDigitalWritePortB<5>(1);
+    delay(200);
+    fastDigitalWritePortB<5>(0);
+    delay(200);
   }
 }
 
@@ -124,13 +140,16 @@ void loop()
   running = digitalRead(A0);
   //TODO debounce 
 
+  int prevClockPeriod24PPQ = clockPeriod24PPQ;
   // clock period: range 10 msec ~ 240 BPM / 60 msec ~ 40 BPM , default 20 ~ 120BPM
   int speedPot = analogRead(A1);
   int currentClockPeriod24PPQ = map(speedPot, 0, 1023, 60, 10);
   // smooth
-  clockPeriod24PPQ = (7*clockPeriod24PPQ + currentClockPeriod24PPQ) / 8;
+  int newClockPeriod24PPQ = (3*prevClockPeriod24PPQ + currentClockPeriod24PPQ) / 4;
+  //Assign to volatile
+  clockPeriod24PPQ = newClockPeriod24PPQ;
 
-  oninterrupt();
+//  oninterrupt();
 
   delay(1);//resolution 1 msec
 }
