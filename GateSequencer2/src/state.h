986c2689d80c7159ee468b5b5d4de0988870ee27
djeemie2000 <peter.jamee@telenet.be>
2022-05-21 11:37:33 +0200

    GateSequencer: state in separate header.

--- GateSequencer2/src/state.h
+++ GateSequencer2/src/state.h
@@ -1,3 +1,63 @@
 #pragma once
 #include <Arduino.h>
 
+// descriptor for a single pattern (8 tracks)
+struct Pattern
+{
+  static const int NumTracks = 8;
+  static const int NumSteps = 32;
+  uint8_t steps[NumSteps];// 1 bit per track
+  uint8_t playMute;// 1 bit per track
+  uint8_t link;// 1 bit per track
+};
+
+// loop state
+struct State
+{
+  // multiple banks?
+  // EEPROM size limited to 512 byts so only 8 patterns = 1 bank possible
+  static const int NumSlots = 8;
+  static const int NumBanks = 1;
+  static const int NumPatterns = NumSlots* NumBanks;
+
+  Pattern pattern[NumPatterns];
+
+  int slot;
+  int bank;
+  int editTrack;//[0-7]
+  int editQuadrant; // [0-3]
+
+  State()
+   : slot(0)
+   , bank(0)
+   , editTrack(0)
+   , editQuadrant(0)
+  {
+    // default pattern for debugging/testing 
+    pattern[0].playMute = 0xFF;//all tracks playing?
+    pattern[0].link = 0x00;//no links
+    for(int step = 0; step<Pattern::NumSteps; ++step)
+    {
+      pattern[0].steps[step] = step<<3;
+    }
+  }
+};
+
+//shared state between interrupt and loop
+struct SharedState
+{
+  Pattern* currentPattern;
+  int currentStep;//[0,31]
+
+  SharedState()
+   : currentPattern(0)
+   , currentStep(0)
+  {}
+};
+
+struct InterruptState
+{
+  int clockIn;
+  int resetIn;
+  //uint8_t clockCounter;//??
+};
