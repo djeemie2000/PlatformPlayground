2a9447d6f7d8f40782c7bc7e53defe7030300ebf
djeemie2000 <peter.jamee@telenet.be>
2023-03-03 15:59:41 +0100

    ClockProcessor: update.

--- DigiNoise/src/dualclockquantizer.h
+++ DigiNoise/src/dualclockquantizer.h
@@ -22,7 +22,7 @@ struct DualClockQuantizer
 
     void setup()
     {
-        m_Cntr = 0;
+        // m_Cntr = 0;
 
         attiny_pin_mode(ClockOutPin1, ATTINY_OUTPUT);
         attiny_pin_mode(ClockOutPin2, ATTINY_OUTPUT);
@@ -43,15 +43,15 @@ struct DualClockQuantizer
     void loop()
     {
         // Update cntr / clock
-        uint32_t period = 256;
-        if (m_Cntr < period)
-        {
-            ++m_Cntr;
-        }
-        else
-        {
-            m_Cntr = 0;
-        }
+        // uint32_t period = 256;
+        // if (m_Cntr < period)
+        // {
+        //     ++m_Cntr;
+        // }
+        // else
+        // {
+        //     m_Cntr = 0;
+        // }
 
         uint32_t cv1 = attiny_analog_read(CV1InPin);
         uint32_t cv2 = attiny_analog_read(CV2InPin);
@@ -74,8 +74,10 @@ struct DualClockQuantizer
             cv2 = 0;
         }
 
-        uint8_t clockInValue = m_Cntr < (period >> 1) ? 1 : 0;
-        // TODO m_ClockIn.Get();
+        //        uint8_t clockInValue = m_Cntr < (period >> 1) ? 1 : 0;
+
+        m_ClockIn.read();
+        uint8_t clockInValue = m_ClockIn.Get();
 
         int out1 = m_State1.Tick(clockInValue, cv1);
         int out2 = m_State2.Tick(clockInValue, cv2);
@@ -87,7 +89,7 @@ struct DualClockQuantizer
     }
 
     GateIn m_ClockIn;
-    uint32_t m_Cntr;
+    // uint32_t m_Cntr;
     CVClockState m_State1;
     CVClockState m_State2;
 };
